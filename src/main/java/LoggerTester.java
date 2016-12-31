bc7cf8a97ce701dc4e088c03a5c3ce2dd6fff690
k0r0tk0ff <korotkov34reg@gmail.com>
2016-12-31 20:38:07 +0300

    work commit

--- src/main/java/LoggerTester.java
+++ src/main/java/LoggerTester.java
@@ -9,17 +9,18 @@ import org.slf4j.LoggerFactory;
  * @version 1.0
  */
 public class LoggerTester {
-    final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    private static final Logger log = LoggerFactory.getLogger(LoggerTester.class);
     Integer temperature;
     Integer oldT;
 
     public void setTemperature(Integer newTemperature) {
         oldT = temperature;
         temperature = newTemperature;
-        logger.debug("Temperature set to {}, old temperature is {}.", temperature, oldT);
+        log.debug("Temperature set to {}, old temperature is {}.", temperature, oldT);
 
             if(temperature.intValue() > 50) {
-                logger.info("Temperature has risen above 50 degrees.");
+                log.info("Temperature has risen above 50 degrees.");
             }
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,3 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**.
  * main class
  * @author k0r0tk0ff
@@ -9,17 +6,8 @@ import org.slf4j.LoggerFactory;
  */
 public class Main {
 
-    //final Logger logger = LoggerFactory.getLogger(Main.class);
-
-    // from https://habrahabr.ru/post/247647
-    //java.util.logging.Logger log4jLog = java.util.logging.getLogger("log4Log");
-
-
-    // http://www.slf4j.org/manual.html
-    //final Logger logger = LoggerFactory.getLogger(Main.class);
-
     public static void main(String[] args) {
         LoggerTester tester = new LoggerTester();
-        tester.setTemperature(30);
+        tester.setTemperature(100);
     }
 }
